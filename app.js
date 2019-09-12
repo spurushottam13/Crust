376d02185542773a3ebe451c5475722a732a76a2
Purushottam Shukla <formalspace@gmail.com>
2019-09-13 00:45:03 +0530

    df

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ var crustRevolve = require('./crust.js');
 
 console.log("ServerR - running ", new Date().getTime())
 
-crustRevolve();
+var http = require('http')
+var server = http.createServer(function(req,res){
+    console.log("request made ",req.url)
+    res.writeHead(200,{'Content-type':'text/plain'})
+    res.end("Hey ")
+})
 
+server.listen(process.env.PORT || 5000)
+
+crustRevolve();
